b2ff0824d2b31c91c4206b6a40c4362baf1175a2
Lu Mingjun_21455461 <136236011+TashLuMJ@users.noreply.github.com>
2023-12-15 04:20:51 +1100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -14,6 +14,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
+// ADD THE NEW ROUTE HERE
+app.get('/', (req, res) => {
+    res, send('My name is Mingjun Lu, I am student at Curtin.');
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
